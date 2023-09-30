471663bff51a0c832a55971eb3a9f622b5847632
Rob Patro <rob-p@users.noreply.github.com>
2023-09-29 22:42:58 -0400

    add actions

--- src/lib.rs
+++ src/lib.rs
@@ -1,14 +1,30 @@
-pub fn add(left: usize, right: usize) -> usize {
-    left + right
+use std::ffi::{OsStr, OsString};
+use std::path::PathBuf;
+
+pub trait WithAdditionalExtension
+    where Self: Into<OsString> {
+    fn with_additional_extension(&self, s: impl AsRef<OsStr>) -> Self;
+}
+
+impl WithAdditionalExtension for PathBuf {
+    fn with_additional_extension(&self, s: impl AsRef<OsStr>) -> Self {
+        // from: https://stackoverflow.com/questions/74322541/how-to-append-to-pathbuf
+        let mut p: OsString = self.into();
+        p.push(s);
+        p.into()
+    } 
 }
 
 #[cfg(test)]
 mod tests {
+    use std::str::FromStr;
     use super::*;
 
     #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
+    fn can_append_extension() {
+        let initial = PathBuf::from_str("foo.bar").unwrap();
+        let updated = initial.with_additional_extension(".baz");
+        eprintln!("u : {:?}", updated);
+        assert_eq!(updated, PathBuf::from_str("foo.bar.baz").unwrap());
     }
 }
