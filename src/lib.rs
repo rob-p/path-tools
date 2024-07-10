30921f1fb16e1e9f5c5ae25cf046f431d96d4bb2
Rob Patro <rob@newton>
2024-07-10 17:21:19 -0400

    feat: working on conversion from PathBuf/Path to CString

--- src/lib.rs
+++ src/lib.rs
@@ -1,4 +1,5 @@
-use std::ffi::{OsStr, OsString};
+use anyhow::anyhow;
+use std::ffi::{CString, OsStr, OsString};
 use std::path::PathBuf;
 
 pub trait WithAdditionalExtension
@@ -14,6 +15,42 @@ impl WithAdditionalExtension for PathBuf {
         let mut p: OsString = self.into();
         p.push(s);
         p.into()
+    }
+}
+
+trait IntoCString {
+    fn into_c_string(&self) -> CString;
+}
+
+impl IntoCString for PathBuf {
+    fn into_c_string(&self) -> CString {
+        #[cfg(not(target_os = "windows"))]
+        {
+            CString::new(std::os::unix::ffi::OsStrExt::as_bytes(
+                <PathBuf as Clone>::clone(self).into_os_string().as_os_str(),
+            ))
+            .unwrap()
+        }
+        #[cfg(target_os = "windows")]
+        CString::new(self.into_os_string().to_str().unwrap()).unwrap()
+    }
+}
+
+trait TryIntoCString {
+    fn try_into_c_string(&self) -> anyhow::Result<CString>;
+}
+
+impl TryIntoCString for PathBuf {
+    fn try_into_c_string(&self) -> anyhow::Result<CString> {
+        #[cfg(not(target_os = "windows"))]
+        {
+            CString::new(std::os::unix::ffi::OsStrExt::as_bytes(
+                <PathBuf as Clone>::clone(self).into_os_string().as_os_str(),
+            ))
+            .map_err(|_e| anyhow!("encountered NulError converting PathBuf to CString"))
+        }
+        #[cfg(target_os = "windows")]
+        CString::new(self.into_os_string().to_str()?)
     }
 }
 
